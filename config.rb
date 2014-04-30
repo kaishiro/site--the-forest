ed4b80b996b48ea35a9881202558d514e801e238
Matthew White <matt@substructu.re>
2014-04-29 20:19:57 -0400

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,51 @@
+require 'redcarpet'
+
+# Load local ENV vars, for development
+require './env' if File.exists?('env.rb')
+
+set :markdown_engine, :redcarpet
+activate :directory_indexes
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+activate :livereload
+
+helpers do
+  def markdown(source)
+    Tilt::KramdownTemplate.new { source }.render
+  end
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = ENV['S3_BUCKET']
+  s3_sync.region                     = ENV['S3_REGION']
+  s3_sync.aws_access_key_id          = ENV['S3_KEY']
+  s3_sync.aws_secret_access_key      = ENV['S3_SECRET']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default. 
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false 
+end
