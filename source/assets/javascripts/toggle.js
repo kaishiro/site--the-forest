c155128a42e66d04342ed21e9014860ea1c13a7f
Matthew White <matt@substructu.re>
2014-08-25 05:02:28 -0400

    Modifying toggle JS to include component deactivation

--- source/assets/javascripts/toggle.js
+++ source/assets/javascripts/toggle.js
@@ -20,8 +20,10 @@
 
       toggle_component: function(e) {
         var ariaPressed = $(this).attr('aria-pressed');
-        var target = $(this).data('target');
+        var activate = $(this).data('activate').split(" ");
+        var deactivate = $(this).data('deactivate').split(" ");
 
+        // Toggle ARIA Role
         if (ariaPressed == 'true') {
           $(this).attr('aria-pressed', 'false');
         }
@@ -29,11 +31,19 @@
           $(this).attr('aria-pressed', 'true');
         }
 
-        if ($('.page.active--' + target).length) {
-          $('.page').removeClass('active--' + target);
+        // Activate Components
+        for (var i=0, l=activate.length; i<l; i++) {
+          if ($('.page.active--' + activate[i]).length) {
+            $('.page').removeClass('active--' + activate[i]);
+          }
+          else {
+            $('.page').addClass('active--' + activate[i]);
+          }
         }
-        else {
-          $('.page').addClass('active--' + target);
+
+        // Deactivate Components
+        for (var i=0, l=deactivate.length; i<l; i++) {
+          $('.page').removeClass('active--' + deactivate[i]);
         }
       }
     }
